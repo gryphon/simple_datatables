38adaac8f709fa88c7bd0ed16b3a067ef6d247a9
Grigory <grigory@snsk.ru>
2011-08-09 19:37:33 +0400

    Engine added

--- lib/simple_datatables.rb
+++ lib/simple_datatables.rb
@@ -1,1 +1,9 @@
+require 'rails'
+
 Mime::Type.register "application/json", :datatables
+
+module SimpleDatatables
+  module Rails
+    require 'simple_datatables/engine'
+  end
+end
---
+++ lib/simple_datatables/engine.rb
@@ -0,0 +1,8 @@
+require 'rails'
+
+module SimpleDatatables
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
