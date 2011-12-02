6b0308781bf5277b82f677249cab1cb0c90e50b3
Sakchai Siripanyawuth <sakchai@artellectual.com>
2011-12-02 14:30:56 +0700

    fnSetFilteringDisplayFix
    Signed-off-by: Sakchai Siripanyawuth <sakchai@artellectual.com>

--- vendor/assets/javascripts/jquery.datatables.fnSetFilteringDelay.js
+++ vendor/assets/javascripts/jquery.datatables.fnSetFilteringDelay.js
@@ -1,40 +1,30 @@
 jQuery.fn.dataTableExt.oApi.fnSetFilteringDelay = function ( oSettings, iDelay ) {
-  /*
-   * Inputs:      object:oSettings - dataTables settings object - automatically given
-   *              integer:iDelay - delay in milliseconds
-   * Usage:       $('#example').dataTable().fnSetFilteringDelay(250);
-   * Author:      Zygimantas Berziunas (www.zygimantas.com) and Allan Jardine
-   * License:     GPL v2 or BSD 3 point style
-   * Contact:     zygimantas.berziunas /AT\ hotmail.com
-   */
-  var
-    _that = this,
-    iDelay = (typeof iDelay == 'undefined') ? 250 : iDelay;
-  
-  this.each( function ( i ) {
-    $.fn.dataTableExt.iApiIndex = i;
-    var
-      $this = this, 
-      oTimerId = null, 
-      sPreviousSearch = null,
-      anControl = $( 'input', _that.fnSettings().aanFeatures.f );
-    
-      anControl.unbind( 'keyup' ).bind( 'keyup', function() {
-      var $$this = $this;
-
-      if (sPreviousSearch === null || sPreviousSearch != anControl.val()) {
-        window.clearTimeout(oTimerId);
-        sPreviousSearch = anControl.val();  
-        oTimerId = window.setTimeout(function() {
-          $.fn.dataTableExt.iApiIndex = i;
-          _that.fnFilter( anControl.val() );
-        }, iDelay);
-      }
-    });
-    
-    return this;
-  } );
-  return this;
-}
-
-
+        var _that = this;
+        this.each( function ( i ) {
+                $.fn.dataTableExt.iApiIndex = i;
+                iDelay  = (iDelay && (/^[0-9]+$/.test(iDelay))) ? iDelay : 250;
+                
+                var $this = this, oTimerId;
+                var anControl = $( 'input', _that.fnSettings().aanFeatures.f );
+                
+                anControl.unbind( 'keyup' ).bind( 'keyup', function(event) {
+                        window.clearTimeout(oTimerId);
+                        
+                        if (event.keyCode == '13') {
+                                // cr, we filter immedately
+                                $.fn.dataTableExt.iApiIndex = i;
+                                _that.fnFilter( $(this).val() );
+                        } else {
+                                // not cr, set new timer
+                                oTimerId = window.setTimeout(function() {
+                                        $.fn.dataTableExt.iApiIndex = i;
+                                        _that.fnFilter( $(this).val() );
+                                }, iDelay);
+                        }
+                        
+                });
+                
+                return this;
+        } );
+        return this;
+};
